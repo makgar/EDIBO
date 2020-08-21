8c7181791ec362d3967ac3781b47caafe40fe6b7
Maksims Garipovs <maksim.garipov@gmail.com>
2020-08-21 03:45:38 +0300

    20200821_03_45_upload_from_cli

--- Pascals_triangle/Pascals_triangle_in_Csharp/Program.cs
+++ Pascals_triangle/Pascals_triangle_in_Csharp/Program.cs
@@ -8,55 +8,33 @@ namespace Pascals_triangle_in_Csharp
         {
             uint n;
             Console.WriteLine("How many rows of Pascals's triangle do you want to see?");
-            while (!uint.TryParse(Console.ReadLine(), out n))
+            while ((!uint.TryParse(Console.ReadLine(), out n)) || n == 0)
             {
                 Console.WriteLine(Environment.NewLine + "The value you have provided is not a positive integer." + Environment.NewLine + "Please try again.");
             }
 
-            int[,] pT;
-            switch (n)
+            int[,] pT = new int[n + 1, n + 1];
+            pT[1, 1] = 1;
+
+            for (int i = 2; i <= n; i++)
             {
-                case 0:
-                    Console.WriteLine("Zero it is then. Chao fow now!");
-                    break;
-                case 1:
-                    pT = new int[1, 1] { { 1 } };
-                    break;
-                case 2:
-                    pT = new int[2, 2];
-                    pT[0, 0] = 1;
-                    pT[1, 0] = 1;
-                    pT[1, 1] = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    pT[i, j] = pT[i - 1, j] + pT[i - 1, j - 1];
+                }
+            }
 
-                    for (int i = 0; i < n; i++)
+            Console.WriteLine(Environment.NewLine + string.Format("Pascal's triangle until row {0} looks as follows:", n));
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (pT[i,j] != 0)
                     {
-                        for (int j = 0; j < n; j++)
-                        {
-                            if (pT[i,j] == 0)
-                            {
-                                continue;
-                            }
-                            Console.Write(string.Format("{0} ", pT[i, j]));
-                        }
-                        Console.Write(Environment.NewLine);
+                        Console.Write(string.Format("{0} ", pT[i, j]));
                     }
-                    break;
-                default:
-                    pT = new int[n, n];
-
-                    for (int i = 0; i < n; i++)
-                    {
-                        for (int j = 0; j < n; j++)
-                        {
-                            if (pT[i,j] == 0)
-                            {
-                                continue;
-                            }
-                            Console.Write(string.Format("{0} ", pT[i, j]));
-                        }
-                        Console.Write(Environment.NewLine);
-                    }
-                    break;
+                }
+                Console.Write(Environment.NewLine);
             }
         }
     }
