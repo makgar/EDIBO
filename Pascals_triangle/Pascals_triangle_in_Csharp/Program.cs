a53289012ad957558422819728433fd5f544b202
Maksims Garipovs <maksim.garipov@gmail.com>
2020-08-21 17:03:02 +0300

    20200821_17_03_upload_from_cli

--- Pascals_triangle/Pascals_triangle_in_Csharp/Program.cs
+++ Pascals_triangle/Pascals_triangle_in_Csharp/Program.cs
@@ -25,8 +25,10 @@ namespace Pascals_triangle_in_Csharp
             }
 
             Console.WriteLine(Environment.NewLine + string.Format("Pascal's triangle until row {0} looks as follows:", n));
+            //for (int i = 0; i <= n; i++)
             for (int i = 1; i <= n; i++)
             {
+                //for (int j = 0; j <= n; j++)
                 for (int j = 1; j <= i; j++)
                 {
                     if (pT[i,j] != 0)
